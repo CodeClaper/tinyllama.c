0e25082ed85998b197efea82fbb51c23ea3ee33e
zhouchao03 <jerryzhou.zc@gmail.com>
2026-06-26 22:19:50 +0800

    feat: add ERRRET.

--- src/slog.h
+++ src/slog.h
@@ -6,5 +6,11 @@ typedef enum {
     ERROR,      /* User error, will abort transaction. */
 } LogLevel;
 
+#define ERRRET(ret_val, ...)   \
+    do {                            \
+        slog(ERROR, __VA_ARGS__); \
+        return(ret_val);            \
+    } while (0)
+
 void slog(LogLevel level, char *format, ...);
 void slog_errno(char *format, ...);
