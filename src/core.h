700be6fa1253c2f8b86e798d657d14eec2b33426
zhouchao03 <jerryzhou.zc@gmail.com>
2026-06-27 23:31:12 +0800

    style: code style change.

--- src/core.h
+++ src/core.h
@@ -1,9 +1,9 @@
 #include "def.h"
 
-bool     model_get_i32(Model *m, const char *key, i32 *out);
-Model   *model_load(const char *path);
-Engine  *engine_open(EngineOptons *opts);
-void     engine_close(Engine *en);
-void     engine_summary(Engine *en);
+bool model_get_i32(Model *m, const char *key, i32 *out);
+Model *model_load(const char *path);
+Engine *engine_open(EngineOptons *opts);
+void engine_close(Engine *en);
+void engine_summary(Engine *en);
 Session *session_create(Engine *en, u32 ctx_size);
-void     session_free(Session *s);
+void session_free(Session *s);
