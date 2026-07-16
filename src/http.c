f9fbfe8a0c1381f6ed6f1f68ab7f44f5dffda5e9
zhouchao03 <jerryzhou.zc@gmail.com>
2026-07-16 22:31:16 +0800

    style: remove unused code.

--- src/http.c
+++ src/http.c
@@ -1,6 +1,5 @@
 #include "http.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
--- src/model/qwen2.c
+++ src/model/qwen2.c
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "model.h"
 #include "../core.h"
