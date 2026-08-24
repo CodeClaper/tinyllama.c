f96c2a3d346d36bd9c2fb55e2de3e6e1e5dcc808
zhouchao03 <jerryzhou.zc@gmail.com>
2026-08-24 23:33:59 +0800

    feat: add asctx.

---
+++ src/asctx.c
@@ -0,0 +1,283 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include "asctx.h"
+#include "def.h"
+
+const uint8_t leftmost_one_pos[256] = {
+	0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+	4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+	5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+	5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+	6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+	6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+	6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+	6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+	7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
+};
+
+/* Leftmost 32bit word postion. */
+static int leftmost_32_pos(uint32_t word) {
+    int shift = 32 - 8;
+    while ((word >> shift) == 0)
+        shift -=8;
+    return shift + leftmost_one_pos[(word >> shift) & 255];
+}
+
+/* Check if the pinter is align. */
+static int is_aligned(void *ptr, size_t alignment) {
+      return ((uintptr_t)ptr & (alignment - 1)) == 0;
+}
+
+static void fatal(const char *msg) {
+    fprintf(stderr, "Fatal for: %s\n", msg);
+    exit(1);
+}
+
+/* Index of AllocSet free size 
+ * --------------------------
+ * At this pointer we must compute ceil(log2(size >> ALLOC_MINBITS))
+ * This is the same as
+ *      leftmost_32_pos((size - 1) >> ALLOC_MINBITS) + 1
+ * or equivalently
+ *      leftmost_32_pos(size - 1) - ALLOC_MINBITS + 1
+ * -------------------------
+ * */
+static inline int AllocSetFreeIndex(size_t size) {
+    int idx = size > (1 << ALLOC_MINBITS) 
+            ? leftmost_32_pos(size -1) - ALLOC_MINBITS + 1
+            : 0;
+    return idx;
+}
+
+static inline void AllocChunkSetMaskExternal(AllocChunk chunk) {
+    chunk->mask = ALLOC_CHUNK_MAGIC | (((uint64_t) 1) << ALLOC_CHUNK_EXTERNAL_BASEBIT);
+}
+
+static inline void AllocChunkSetMask(AllocChunk chunk, AllocBlock block, size_t value) {
+    size_t offset = (char *) chunk - (char *) block;
+    chunk->mask = (((uint64_t) offset) << ALLOC_CHUNK_BLOCK_OFFSET_BASEBIT) | ((uint64_t) value) << ALLOC_CHUNK_VALUE_BASEBIT;
+}
+
+static inline bool AllocChunkIsExternal(AllocChunk chunk) {
+    return CHUNK_IS_EXTERNAL(chunk->mask);
+}
+
+static inline AllocBlock AllocChunkGetBlock(AllocChunk chunk) {
+    return (AllocBlock) ((char *) chunk - CHUNK_GET_OFFSET(chunk->mask));
+}
+
+AllocSetContext *AllocSetMemoryContextCreate(u32 block_size) {
+    AllocSetContext *set;
+    size_t size;
+    AllocBlock block;
+
+    size = ALLOC_SET_CXT_SIZE + ALLOC_BLOCK_SIZE + ALLOC_CHUNK_SIZE;
+    size = MAXALIGN(size);
+    
+    set = (AllocSet) malloc(size);
+    if (!set) fatal("Out of memory");
+
+    block          = KEEPER_ALLOC_BLOCK(set);
+    block->set     = set;
+    block->pres    = NULL;
+    block->next    = NULL;
+    block->freeptr = ((char *) block) + ALLOC_BLOCK_SIZE;
+    block->endptr  = ((char *) set) + size;
+
+    set->blocks = block;
+    set->next_block_size = size;
+    memset(set->free_list, 0, sizeof(set->free_list));
+
+    return set;
+}
+
+void *AllocSetAllocChunkFromBlock(AllocSetContext *context, AllocBlock block, size_t chksize) {
+    AllocChunk chunk = (AllocChunk) block->freeptr;
+    block->freeptr += (chksize + ALLOC_CHUNK_SIZE);
+    chunk->next = NULL;
+    AllocChunkSetMask(chunk, block, chksize);
+    return CHUNK_GET_POINTER(chunk);
+}
+
+static void *AllocSetAllocNewBlock(AllocSetContext *context, size_t chksize) {
+    size_t blk_size;
+    AllocSet set = (AllocSet) context;
+
+    blk_size = set->next_block_size;
+    AllocBlock block = (AllocBlock) malloc(blk_size);
+    if (!block) fatal("Out of memory");
+
+    block->freeptr = ((char *) block) + ALLOC_BLOCK_SIZE;
+    block->endptr  = ((char *) block) + blk_size;
+    block->set     = set;
+    block->next    = set->blocks;
+    block->pres    = NULL;
+    
+    if (block->next) block->next->pres = block;
+    set->blocks = block;
+
+    return AllocSetAllocChunkFromBlock(context, block, chksize);
+}
+
+static void *AllocSetAllocLarge(AllocSetContext *context, size_t size) {
+    AllocSet set = (AllocSet) context;
+    size_t blk_size;
+    AllocBlock block;
+    AllocChunk chunk;
+    
+    blk_size = MAXALIGN(size + ALLOC_BLOCK_SIZE + ALLOC_CHUNK_SIZE);
+    block = (AllocBlock) malloc(blk_size);
+    if (!block) fatal("Out of memory");
+
+    block->set = set;
+    block->freeptr = block->endptr = ((char *) block) + blk_size;
+    chunk = (AllocChunk) (((char *) block) + ALLOC_BLOCK_SIZE);
+    /* Mark the Chunk as externally managed. */
+    AllocChunkSetMaskExternal(chunk);
+
+    if (set->blocks != NULL) {
+        block->pres = set->blocks;
+        block->next = set->blocks->next;
+        if (block->next != NULL) block->next->pres = block;
+        set->blocks->next = block;
+    } else {
+        block->pres = NULL;
+        block->next = NULL;
+        set->blocks = block;
+    }
+
+    return CHUNK_GET_POINTER(chunk);
+}
+
+void *AllocSetAlloc(AllocSetContext *context, size_t size) {
+    size_t     chksize, freesize;
+    int        fdx;
+    AllocChunk chunk;
+
+    if (size > ALLOC_CHUNK_LIMIT) return AllocSetAllocLarge(context, size);
+
+    AllocSet   set   = (AllocSet) context;
+    AllocBlock block = set->blocks;
+
+    /* Find there is enough chunk in FreeList. */
+    fdx = AllocSetFreeIndex(size);
+    chunk = set->free_list[fdx];
+    if (chunk) {
+        set->free_list[fdx] = chunk->next;
+        return CHUNK_GET_POINTER(chunk);
+    }
+
+    chksize = CHUNK_GET_SIZE_FROM_FREE_LIST_IDX(fdx);
+
+    /* Get block free space size. 
+     * If it has enough free space, allocate from current block. 
+     * Otherwise, generate new block and allocate from that. */
+    freesize = block->endptr - block->freeptr;
+
+    if (freesize < chksize + ALLOC_CHUNK_SIZE) return AllocSetAllocNewBlock(context, chksize);
+    else return AllocSetAllocChunkFromBlock(context, block, chksize);
+}
+
+void *AllocSetRealloc(void *ptr, size_t size) {
+    AllocChunk chunk = POINTER_GET_CHUNK(ptr);
+    AllocBlock block;
+    AllocSet   set;
+    size_t     old_size;
+
+    /* Way to external chunk. */
+    if (AllocChunkIsExternal(chunk)) {
+        size_t blksize;
+        size_t oldblksize;
+
+        block = CHUNK_EXTERNAL_GET_BLOCK(chunk);
+        set = block->set;
+        blksize = MAXALIGN(size + ALLOC_BLOCK_SIZE + ALLOC_CHUNK_SIZE);
+        oldblksize = block->endptr - ((char *) block);
+
+        block = realloc(block, blksize);
+        if (!block) fatal("Out of memory");
+
+        block->freeptr = block->endptr = ((char *) block) + blksize;
+        chunk = (AllocChunkData *) (((char *) block) + ALLOC_BLOCK_SIZE);
+
+        if (block->pres) block->pres->next = block;
+        else set->blocks = block;
+        if (block->next) block->next->pres = block;
+
+        return CHUNK_GET_POINTER(chunk);
+    }
+
+    /* Way to normal chunk. */
+    block = AllocChunkGetBlock(chunk);
+    set = block->set;
+    old_size = CHUNK_GET_VALUE(chunk->mask);
+
+    if (old_size >= size) return ptr; 
+    else {
+        void *nptr = AllocSetAlloc((AllocSetContext *) set, size);
+        memcpy(nptr, ptr, old_size);
+        AllocSetFree(ptr);
+        return nptr;
+    }
+}
+
+void AllocSetFree(void *ptr) {
+    AllocSet   set;
+    AllocChunk chunk = POINTER_GET_CHUNK(ptr);
+
+    if (AllocChunkIsExternal(chunk)) {
+        AllocBlock block = CHUNK_EXTERNAL_GET_BLOCK(chunk);
+        set = block->set;
+        if (block->pres) block->pres->next = block->next;
+        else set->blocks = block->next;
+        if (block->next) block->next->pres = block->pres;
+        free(block);
+    } else {
+        AllocBlock block = AllocChunkGetBlock(chunk); 
+        set = block->set;
+        int fdx = AllocSetFreeIndex(CHUNK_GET_VALUE(chunk->mask));
+        chunk->next = set->free_list[fdx];
+        set->free_list[fdx] = chunk;
+    }
+}
+
+void AllocSetReset(AllocSetContext *context) {
+    AllocSet   set        = (AllocSet) context;
+    size_t     keepersize = KEEPER_ALLOC_BLOCK(set)->endptr - ((char *) set);
+    AllocBlock block      = set->blocks;
+
+    /* Clean the free list*/
+    memset(set->free_list, 0, sizeof(set->free_list));
+
+    /* Reset first block as header. */
+    set->blocks = KEEPER_ALLOC_BLOCK(set);
+
+    while (block != NULL) {
+        AllocBlock next = block->next;
+        if (IS_KEEPER_BLOCK(set, block)) {
+            /* Way to handle first block. */
+            char *start = ((char *) block) + ALLOC_BLOCK_SIZE;
+            block->freeptr = start;
+            block->next = NULL;
+            block->pres = NULL;
+        } else {
+            /* Way to handle other block. */
+            free(block);
+        }
+        block = next;
+    }
+}
+
+void AllocSetDelete(AllocSetContext *context) {
+    AllocSetReset(context);
+    free((AllocSet) context);
+    context = NULL;
+}
---
+++ src/asctx.h
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include "def.h"
+
+typedef struct AllocSetContext  *AllocSet;
+typedef struct AllocBlockData   *AllocBlock;
+typedef struct AllocChunkData   *AllocChunk;
+
+#define ALLOC_MINBITS                           3   /* Smallest chunk size is 8 bytes. */
+#define ALLOC_FREELISTS_NUM                     11  /* FreeLists num. */
+#define ALLOC_CHUNK_LIMIT                       1 << (ALLOC_FREELISTS_NUM + ALLOC_MINBITS - 1)  /* Size of largest chunk. */
+#define ALLOCSET_NUM_FREELISTS                  11
+#define DEFAULT_MAX_BLOCK_SIZE                  8 * 1024 * 1024
+#define ALLOC_CHUNK_EXTERNAL_BASEBIT            3
+#define ALLOC_CHUNK_VALUE_BASEBIT               (ALLOC_CHUNK_EXTERNAL_BASEBIT + 1)
+#define ALLOC_CHUNK_BLOCK_OFFSET_BASEBIT        (ALLOC_CHUNK_VALUE_BASEBIT + 30)
+#define ALLOC_CHUNK_MAX_VALUE                   UINT64CONST(0x3FFFFFFF)
+#define ALLOC_CHUNK_MAX_OFFSET                  UINT64CONST(0x3FFFFFFF)
+#define ALLOC_CHUNK_BLOCK_OFFSET_MASK           UINT64CONST(0x3FFFFFFF)
+#define ALLOC_CHUNK_MAGIC                       (UINT64CONST(0xBDEADB858E77BABA) >> ALLOC_CHUNK_VALUE_BASEBIT << ALLOC_CHUNK_VALUE_BASEBIT)
+#define ALLOC_SET_CXT_SIZE                      sizeof(AllocSetContext)
+#define ALLOC_BLOCK_SIZE                        sizeof(AllocBlockData)
+#define ALLOC_CHUNK_SIZE                        sizeof(AllocChunkData)
+#define KEEPER_ALLOC_BLOCK(set)                 ((AllocBlockData *) ((char *) set + ALLOC_SET_CXT_SIZE))
+#define IS_KEEPER_BLOCK(set, block)             (block == (KEEPER_ALLOC_BLOCK(set)))
+#define CHUNK_GET_POINTER(chunk)                ((void *) ((char *) chunk + ALLOC_CHUNK_SIZE))
+#define POINTER_GET_CHUNK(ptr)                  ((AllocChunkData *) ((char *) ptr - ALLOC_CHUNK_SIZE))
+#define CHUNK_EXTERNAL_GET_BLOCK(chunk)         ((AllocBlockData *) ((char *) chunk - ALLOC_BLOCK_SIZE))
+#define CHUNK_GET_SIZE_FROM_FREE_LIST_IDX(fdx)  ((((size_t) 1) << ALLOC_MINBITS) << (fdx))
+#define CHUNK_IS_EXTERNAL(mask)                 (mask & (((uint64_t) 1) << ALLOC_CHUNK_EXTERNAL_BASEBIT))
+#define CHUNK_CHECK_MAGIC(mask)                 (ALLOC_CHUNK_MAGIC == (mask >> ALLOC_CHUNK_VALUE_BASEBIT << ALLOC_CHUNK_VALUE_BASEBIT))
+#define CHUNK_GET_OFFSET(mask)                  ((mask >> ALLOC_CHUNK_BLOCK_OFFSET_BASEBIT) & ALLOC_CHUNK_BLOCK_OFFSET_MASK)
+#define CHUNK_GET_VALUE(mask)                   ((mask >> ALLOC_CHUNK_VALUE_BASEBIT) & ALLOC_CHUNK_MAX_VALUE)
+#define MAXIMUM_ALIGNOF                         8
+#define TYPEALIGN(ALIGNVAL,LEN)                 (((uintptr_t) (LEN) + ((ALIGNVAL) - 1)) & ~((uintptr_t) ((ALIGNVAL) - 1)))
+#define MAXALIGN(LEN)			                TYPEALIGN(MAXIMUM_ALIGNOF, (LEN))
+#define MAX_MALLOC_SIZE                         (1024 * 1024 * 1024 * 2L)
+
+typedef struct AllocSetContext {
+    AllocBlock  blocks;                              /* Header of AllocBlock list. */
+    AllocChunk  free_list[ALLOCSET_NUM_FREELISTS];   /* Free chunk list. */ 
+    uint32_t    next_block_size;                     /* Next block size used when allocating new block. */
+} AllocSetContext;
+
+typedef struct AllocBlockData {
+    AllocSet    set;
+    AllocBlock  pres;
+    AllocBlock  next;
+    char        *freeptr;       /* Start of free space of this block. */
+    char        *endptr;        /* End of free space of this block. */
+} AllocBlockData;
+
+typedef struct AllocChunkData {
+    AllocChunk  next;           /* Next AllocChunk, use in free list. */
+    u64         mask;           /* Store details of the chunk. */
+} AllocChunkData;
+
+AllocSetContext *AllocSetMemoryContextCreate(u32 block_size);
+void *AllocSetAlloc(AllocSetContext *context, size_t size);
+void *AllocSetRealloc(void *ptr, size_t size);
+void AllocSetFree(void *ptr);
+void AllocSetMemoryContextDelete(AllocSetContext *context);
